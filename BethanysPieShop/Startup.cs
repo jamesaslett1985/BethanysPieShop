18e93b0235209fd95792539068b2ea64a0e5e4c8
jamesaslett1985 <jamesaslett1985@gmail.com>
2020-10-19 15:06:15 +0100

    Bookmark 3.5

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
 
 namespace BethanysPieShop
 {
@@ -16,6 +17,10 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //register framework services
+            services.AddControllersWithViews();
+
+            //register our own services
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,7 +28,15 @@ namespace BethanysPieShop
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                /*middleware components. These run sequentially, therefore the order in which they are added is important. Typically the endpoint call would be at the end
+                and a component to compress the response going back to the client should be added before the use static files component, otherwise the static files would not be compressed
+                */
+
+                app.UseDeveloperExceptionPage(); //gives useful error messaging
+                app.UseStatusCodePages(); //gives useful status codes, eg: 404
+                app.UseStaticFiles(); //use of static files eg: images
+                app.UseEndpoints(endpoints => //enables routing to correct endpoint in the application
+                { });
             }
 
             app.UseRouting();
